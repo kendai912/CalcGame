4d56088c548e69432d22036c7c9363d58daaa7fe
Kendai <kendai912@gmail.com>
2022-12-16 01:26:47 +0900

    Initial Commit

---
+++ CalcGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcGame
+//
+//  Created by Kendai on 2022/12/16.
+//  Copyright © 2022 Kendai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
